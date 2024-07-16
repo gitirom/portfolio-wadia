fac2c4b1ff623e2504eb9ec783cc2922a4ac2d45
gitirom <wadiaromdane10@gmail.com>
2024-07-16 19:31:55 +0200

    building sccess!

--- sitemap-generator.mjs
+++ sitemap-generator.mjs
@@ -14,7 +14,12 @@ const urls = [
 
 const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls.map(url => `<url><loc>${url}</loc></url>`).join('')}
+    ${urls.map(url => `
+    <url>
+        <loc>${url}</loc>
+        <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+        <priority>0.8</priority>
+    </url>`).join('')}
 </urlset>`;
 
 const publicPath = path.resolve('./public');
