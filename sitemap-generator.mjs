1f1b8d4b63057baf4ecd717aed5783f95a7325cf
gitirom <wadiaromdane10@gmail.com>
2024-07-16 18:58:21 +0200

    Submition 2

---
+++ sitemap-generator.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+const urls = [
+    'https://gitirom.github.io/portfolio-wadia/',
+    'https://gitirom.github.io/portfolio-wadia/#about',
+    'https://gitirom.github.io/portfolio-wadia/#skills',
+    'https://gitirom.github.io/portfolio-wadia/#services',
+    'https://gitirom.github.io/portfolio-wadia/#qualification',
+    'https://gitirom.github.io/portfolio-wadia/#work',
+    'https://gitirom.github.io/portfolio-wadia/#testimonials',
+    'https://gitirom.github.io/portfolio-wadia/#contact',
+];
+
+const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.map(url => `<url><loc>${url}</loc></url>`).join('')}
+</urlset>`;
+
+const publicPath = path.resolve('./public');
+
+if (!fs.existsSync(publicPath)){
+    fs.mkdirSync(publicPath);
+}
+
+fs.writeFileSync(path.join(publicPath, 'sitemap.xml'), sitemapContent, 'utf8');
+console.log('Sitemap generated successfully');
